9310c204b1c338e226e50e5dbdd8ebefa44deca8
NoahZinter <noah.zinter@gmail.com>
2021-07-02 11:41:51 -0600

    Set up factorybots

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
+  let!(:todos) { create_list(:todo, 10) }
+  let(:todo_id) {todos.first.id}
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  
   end
 end
--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Todos", type: :request do
+RSpec.describe "Todos API", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    
   end
 end
